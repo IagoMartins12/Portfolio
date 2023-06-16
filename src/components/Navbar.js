3e754d6406dd6e3876fd6181bed17304a0465c69
Iago martins <martinsiagosaraiva@gmail.com>
2023-06-16 16:16:07 -0300

    change navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -108,7 +108,7 @@ function NavBar() {
                 target="_blank"
                 className="fork-btn-inner"
               >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                <CgGitFork style={{ fontSize: "1.2em" }} className="forkIcon" />{" "}
                 <AiFillStar style={{ fontSize: "1.1em" }} />
               </Button>
             </Nav.Item>
