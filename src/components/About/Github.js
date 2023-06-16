4b7f7468a1c78189808a76a79e0524cbcfe33ea5
Iago martins <martinsiagosaraiva@gmail.com>
2023-06-16 16:03:39 -0300

    change portfólio url

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -3,6 +3,24 @@ import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
 
 function Github() {
+  const selectLastHalfYear = contributions => {
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth();
+  const shownMonths = 6;
+
+  return contributions.filter(activity => {
+    const date = new Date(activity.date);
+    const monthOfDay = date.getMonth();
+
+    return (
+      date.getFullYear() === currentYear &&
+      monthOfDay > currentMonth - shownMonths &&
+      monthOfDay <= currentMonth
+    );
+  });
+};
+
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
@@ -15,6 +33,7 @@ function Github() {
         color="#c084f5"
         fontSize={16}
         year={2023}
+        transformData={selectLastHalfYear} 
       />
     </Row>
   );
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
- "https://raw.githubusercontent.com/IagoMartins12/Portfolio/master/src/Assets/curriculo.pdf";
+ "https://raw.githubusercontent.com/IagoMartins12/Portfolio/master/src/Assets/Portfólio.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
