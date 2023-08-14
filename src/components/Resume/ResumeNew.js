c740b3d8e73725e1c97ccc545508a0cff986c0e5
Iago martins <martinsiagosaraiva@gmail.com>
2023-08-14 16:53:48 -0300

    change resume feature

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -6,13 +6,19 @@ import pdf from '../../Assets/curriculo.pdf';
 import { AiOutlineDownload } from 'react-icons/ai';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import useLanguageContext from '../../Context/Language/LanguageContext';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+const resumeLinkEnglish =
+  'https://raw.githubusercontent.com/IagoMartins12/Portfolio2/main/src/Assets/Portf%C3%B3lioIngles.pdf';
+
 const resumeLink =
-  'https://raw.githubusercontent.com/IagoMartins12/Portfolio/master/src/Assets/Portfólio.pdf';
+  'https://raw.githubusercontent.com/IagoMartins12/Portfolio2/main/src/Assets/Portf%C3%B3lioAtt.pdf';
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
+
+  const language = useLanguageContext();
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -35,7 +41,10 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document
+            file={language.isEnglish ? resumeLinkEnglish : resumeLink}
+            className="d-flex justify-content-center"
+          >
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
